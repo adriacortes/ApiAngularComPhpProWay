69ac13adaecb63a5f7fb8877fdb39497e1380e3c
Ádria Aline Castro da Silva Côrtes <adria.aline@gmail.com>
2023-07-04 17:25:52 -0300

    Melhoria ALTERAR.PHP

--- alterar.php
+++ alterar.php
@@ -10,20 +10,31 @@
     $extrair = json_decode($obterDados);
 
 //Separar os dados do JSON
-    $idCurso = $extrair-> cursos-> idCurso; 
-    $nomeCurso = $extrair-> cursos-> nomeCurso;   //cursos é o nome do JSON definido com json_encode
-    $valorCurso = $extrair-> cursos-> valorCurso; 
+	$idCurso    = $extrair->idCurso; 
+	$nomeCurso  = $extrair->nomeCurso;
+    $valorCurso = $extrair->valorCurso;
     
 //SQL
     $sql = "UPDATE cursos SET nomeCurso = '$nomeCurso', valorCurso = $valorCurso WHERE idCurso = $idCurso";
+//Executando o SQL	
     mysqli_query($conexao, $sql);
 
 //Exportar os dados cadastrados
-    $curso = [
+    $curso=[
         'idCurso' => $idCurso,
         'nomeCurso' => $nomeCurso,
         'valorCurso' => $valorCurso
-    ]
-    json_encode(['curso'=>$curso]);
+    ];
+	
+   echo json_encode(['curso'=>$curso]); //apresentar os dados alterados
 
+    //API ok testada no POSTMAN chamada:
+   //http://localhost/php-pris/alterar.php  
+   //dados via JSON
+   //Ex.:
+  // {
+  //  "idCurso":114,
+ //   "nomeCurso": "Java 8",
+  //  "valorCurso": "400"
+  // } 
 ?>
